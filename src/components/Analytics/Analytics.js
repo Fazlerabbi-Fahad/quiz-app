6ee71181f99921e062c97fc74558a6e15b8943fd
Fazle Rabbi Fahad <fazlerabbifahad.54@gmail.com>
2022-10-11 03:08:45 +0600

    analytics header done

--- src/components/Analytics/Analytics.js
+++ src/components/Analytics/Analytics.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import analytics from './../../Images/analytics.jpg';
+import './Analytics.css';
 
 const Analytics = () => {
     return (
         <div>
-            This is analytics
+            <div className='home-header bg-dark'>
+                <img className='img-fluid w-100 opacity-25' src={analytics} alt="" />
+                <div className='position-absolute top-0 text-white home-header-text'>
+                    <h1 className='fw-bold fs-1'>Let's Learn Some Graphs and Chart</h1>
+                    <h2 className='fw-bold fs-1'>Are you ready to test your skill?</h2>
+                </div>
+            </div>
         </div>
     );
 };
