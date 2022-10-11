3dd0c05f90558c00d9ba02239032af751349496c
Fazle Rabbi Fahad <fazlerabbifahad.54@gmail.com>
2022-10-11 10:30:29 +0600

    All done

--- src/components/Questions/Questions.js
+++ src/components/Questions/Questions.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useLoaderData } from 'react-router-dom';
 import Quiz from './../Quiz/Quiz'
 
@@ -6,16 +6,18 @@ const Questions = () => {
     const questions = useLoaderData();
     const question = questions.data;
     const quiz = question.questions;
-    let sum = 0;
+    const [answers, setAnswers] = useState([]);
     const CheckCorrectAnswer = (answer, correctAnswer) => {
         if (answer !== correctAnswer) {
             alert('Your Answer is Wrong');
+
         } else {
-            sum = sum + 1;
+            const newAnswers = [...answers, answer];
+            setAnswers(newAnswers);
         }
-        return sum;
+
     };
-    console.log(sum);
+
 
 
 
@@ -23,11 +25,13 @@ const Questions = () => {
     return (
         <div className='container mt-5'>
             <h1 className='fs-1 fw-bold mb-2'>{question.name}</h1>
-            <h2 className='mb-5'>Total:{question.total}</h2>
+            <h2 className='mb-2'>Total:{question.total}</h2>
+            <h2 className='mb-5'>Correct Answers:{answers.length}</h2>
             {
                 quiz.map(question => <Quiz
                     key={question.id}
                     question={question}
+                    answers={answers}
                     CheckCorrectAnswer={CheckCorrectAnswer}></Quiz>)
             }
             <button className='btn text-decoration-none mb-5 border bg-secondary'><Link className='text-decoration-none text-white' to='/home'>Go to homepage</Link></button>
--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -4,24 +4,40 @@ import { EyeIcon } from '@heroicons/react/24/solid';
 
 const Quiz = (props) => {
     const { question, options, correctAnswer } = props.question;
+    let answer;
+    for (answer of props.answers) {
+        console.log(answer);
+    }
+    console.log(answer);
     const ShowCorrectAnswer = correctAnswer => {
         alert(correctAnswer);
     }
 
     return (
         <div className='border rounded m-5 p-5 quiz'>
-            <div className='d-flex justify-content-between' >
-                <h2>{question}</h2>
-                <EyeIcon title="It's the correct answer" onClick={() => ShowCorrectAnswer(correctAnswer)} className='btn-icon mb-5'></EyeIcon>
+            <div className="container">
+                <div className="row">
+                    <div className='col-10' >
+                        <h2>{question}</h2>
+                    </div>
+                    <div className="col-2">
+                        <EyeIcon title="It's the correct answer" onClick={() => ShowCorrectAnswer(correctAnswer)} className='btn-icon mb-5'></EyeIcon>
+
+                    </div>
+                </div>
             </div>
-            {
-                options.map(option =>
-                    <button className='btn border me-5 mt-3 btn-quiz'
-                        onClick={() => props.CheckCorrectAnswer(option, correctAnswer)}>{option}</button>)
-            }
+            <div className="container">
+                <div className="row">
+                    {
+                        options.map(option =>
+                            <button className='btn border col-5 me-5 mt-3 btn-quiz'
+                                onClick={() => props.CheckCorrectAnswer(option, correctAnswer)}>{option}</button>)
+                    }
+                </div>
+            </div>
 
 
-        </div>
+        </div >
     )
 };
 
